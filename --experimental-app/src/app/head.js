1913ff5ba1e9342e9f23df649fe665b45185078c
Bhanuprakash842 <127642353+Bhanuprakash842@users.noreply.github.com>
2025-01-01 16:37:24 +0530

    installing dependencies of project

---
+++ --experimental-app/src/app/head.js
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+    <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width" />
+        <meta name="description" content="This is Imdb Clone" />
+        <title>IMDB Clone</title>
+        <link rel="icon" href="/favicon.icon" />
+      </head></>
+  );
+}
